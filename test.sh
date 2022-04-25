f8ad3d88fbef388673e5dccb8c9a4140c085f251
Waqas Kayani <waqas.kayani@emumba.com>
2022-04-25 16:47:38 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@
 name=$1
 VAR=$2
 
-sed -i "s~my_key~$(gcloud secrets versions access "latest" --secret=my-$name)~g" /home/ubuntu/test.sh
+sed -i "s~my_key~$(gcloud secrets versions access "latest" --secret=my-$name)~g" /home/ubuntu/test99.sh
 echo $VAR > /etc/test2.txt
