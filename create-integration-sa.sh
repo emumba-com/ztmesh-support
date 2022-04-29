436f1b064bb8641fd0e77cd04571dcdb9dd5827f
Waqas Kayani <waqas.kayani@emumba.com>
2022-04-29 10:59:00 +0500

    Update create-integration-sa.sh

--- create-integration-sa.sh
+++ create-integration-sa.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 PROJECT_ID=$1
+ENV=$2
 
 # Set project for command-line tool
 gcloud config set project $PROJECT_ID
 
 # Update Project-id
+ENV=-$ENV
 sed -i "s~INSERT_PROJECT_ID~$PROJECT_ID~g" service-account.yaml
+sed -i "s~extreme-ztna-sa~extreme-ztna-sa$ENV~g" service-account.yaml
 
 # Create deployment
-gcloud deployment-manager deployments create ztmesh-deployment --config service-account.yaml
+gcloud deployment-manager deployments create ztmesh-deployment$ENV --config service-account.yaml
