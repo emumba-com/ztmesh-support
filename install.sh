70c08b1bb373840e6dcd870e3696aa590b90d7cc
Usama42 <36984210+Usama42@users.noreply.github.com>
2022-07-25 20:36:34 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-# curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/4d65ca99ac4a30585be1fc059d3111600dde7c81/hostagent_install_0.0.1.sh | sudo bash -s $1 
+# curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/4d65ca99ac4a30585be1fc059d3111600dde7c81/hostagent_install_0.0.1.sh | sudo bash -s $1  ; sudo ho
+st-agent-cli start -c -t
