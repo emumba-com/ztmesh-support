e6307bd0167af8006e743e9e84495d32882a9f22
Usama42 <36984210+Usama42@users.noreply.github.com>
2022-07-25 21:10:23 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
-sudo apt-get update -y
-sudo apt install git
-BRANCH="$(git branch --show-current)"
-echo $BRANCH
-if [[ $BRANCH -eq "dev" ]] ; then
+
+if [[ $BRANCH -eq "host-agent" ]] ; then
    curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/host-agent/hostagent_install_0.0.1.sh | sudo bash -s $1  ; sudo ho
 st-agent-cli start -c -t
 fi
