7d14cbda54bf506463d0ff3ce0412ce2e83b1fbc
Usama42 <36984210+Usama42@users.noreply.github.com>
2022-07-26 11:33:16 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 
-if [[ $1 -eq "staging" ]] ; then
+if [[ $1 -eq "dev" ]] ; then
    curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/host-agent/hostagent_install_0.0.1.sh | sudo bash -s $1  ; sudo ho
 st-agent-cli start -c -t
+else 
+   echo "No script"
 fi
 
 
