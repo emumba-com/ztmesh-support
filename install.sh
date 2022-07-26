9d0cf53c4072a06c45cf0b6e6d21bbadf26245fd
Usama42 <36984210+Usama42@users.noreply.github.com>
2022-07-26 12:08:42 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 #/bin/bash
-if [ "$1" == "staging" ]; then
+if [ "$1" == "dev" ]; then
    curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/host-agent/hostagent_install_0.0.1.sh | sudo bash -s $1  ; sudo host-agent-cli start -c -t
+   
+elif [ "$1" == "staging" ]; then
+   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/host-agent/hostagent_install_0.0.1.sh | sudo bash -s $1  ; sudo host-agent-cli start -c -t
+
+elif [ "$1" == "qa" ]; then
+   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/host-agent/hostagent_install_0.0.1.sh | sudo bash -s $1  ; sudo host-agent-cli start -c -t
+
+elif [ "$1" == "beta" ]; then
+   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/host-agent/hostagent_install_0.0.1.sh | sudo bash -s $1  ; sudo host-agent-cli start -c -t
+ 
 else
-   echo $1
+   echo "No environment exists named $1"
+
 fi 
 
  
