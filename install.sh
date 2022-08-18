0e9976eae10fcef17f84b1bd8defaa97af199272
Usama42 <36984210+Usama42@users.noreply.github.com>
2022-08-18 17:08:31 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,25 @@
 #/bin/bash
 if [ "$1" == "dev" ]; then
-   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/relay-agent/relayagent_install_0.0.1.sh | sudo bash -s $1  
+   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/relay-agent/scripts/relayagent_install_0.0.1.sh | sudo bash -s $1  
 elif [ "$1" == "staging" ]; then
-   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/relay-agent/relayagent_install_0.0.1.sh | sudo bash -s $1  
+   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/relay-agent/scripts/relayagent_install_0.0.1.sh | sudo bash -s $1  
 elif [ "$1" == "qa" ]; then
-   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/relay-agent/relayagent_install_0.0.1.sh | sudo bash -s $1
+   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/relay-agent/scripts/relayagent_install_0.0.1.sh | sudo bash -s $1
 
 elif [ "$1" == "beta" ]; then
-   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/relay-agent/relayagent_install_0.0.1.sh | sudo bash -s $1 
+   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/relay-agent/scripts/relayagent_install_0.0.1.sh | sudo bash -s $1 
 
 elif [ "$1" == "beta01" ]; then
-   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/relay-agent/relayagent_install_0.0.1.sh | sudo bash -s $1
+   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/relay-agent/scripts/relayagent_install_0.0.1.sh | sudo bash -s $1
 
 elif [ "$1" == "qa-chaos" ]; then
-   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/relay-agent/relayagent_install_0.0.1.sh | sudo bash -s $1
+   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/relay-agent/scripts/relayagent_install_0.0.1.sh | sudo bash -s $1
 
 elif [ "$1" == "qa-loadtesting" ]; then
-   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/relay-agent/relayagent_install_0.0.1.sh | sudo bash -s $1
+   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/relay-agent/scripts/relayagent_install_0.0.1.sh | sudo bash -s $1
 
 elif [ "$1" == "feature-1" ]; then
-   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/relay-agent/relayagent_install_0.0.1.sh | sudo bash -s $1
+   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/relay-agent/scripts/relayagent_install_0.0.1.sh | sudo bash -s $1
 else
    echo "No environment exists named $1"
 
