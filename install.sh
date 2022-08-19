43619cd22bb8fbca74584d365eba047d629b3134
Usama42 <36984210+Usama42@users.noreply.github.com>
2022-08-19 17:04:23 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ if [ "$1" == "dev" ]; then
    curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/host-agent/hostagent_install_0.0.2.sh | sudo bash -s $1 ; sudo host-agent-cli start -c -t $2
    
 elif [ "$1" == "staging" ]; then
-   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/host-agent/hostagent_install_0.0.1.sh | sudo bash -s $1  ; sudo host-agent-cli start -c -t
+   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/host-agent/hostagent_install_0.0.1.sh | sudo bash -s $1  ; sudo host-agent-cli start -c -t $2
 
 elif [ "$1" == "qa" ]; then
-   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/host-agent/hostagent_install_0.0.2.sh | sudo bash -s $1  ; sudo host-agent-cli start -c -t
+   curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/host-agent/hostagent_install_0.0.2.sh | sudo bash -s $1  ; sudo host-agent-cli start -c -t $2
 
 elif [ "$1" == "beta" ]; then
    curl -L https://raw.githubusercontent.com/extremenetworks/ztmesh-support/host-agent/hostagent_install_0.0.1.sh | sudo bash -s $1  ; sudo host-agent-cli start -c -t
