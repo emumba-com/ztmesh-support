78151470ff784cb4e1b35594193fe67374b2659e
zeeshan50522 <zeeshanwaheed50522@gmail.com>
2022-10-28 15:57:36 +0500

    feature-4-gcp

--- gcp-service-connector-script.sh
+++ gcp-service-connector-script.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-IDENTIFIER=$1
-BUILD_ENV=$2
+INSTANCE_NAME=$1
+VAULT_NAME=$2
+BUILD_ENV=$3
 FILEPATH=/root/.host-agent
 
 apt update -y
+apt install jq -y
+
+# Get access token for Azure Vault audience
+access_token=$(curl -s 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://vault.azure.net' -H Metadata:true | jq -r '.access_token')
 
 echo "Creating config.json in path: ${FILEPATH}"
 
@@ -19,30 +24,34 @@ cat >$FILEPATH/config.json << 'EOL'
   },
   "relay_config": {
     "relay": "value_5",
-    "relay_address": "value_6"
+    "endpoint": "value_6",
+    "id" : "value_8"
   },
   "site_config": {
     "id": "value_7"
-  }
+  },
+  "wc_url": "value_9"
 }
 EOL
 
 
-sed -i "s~value_1~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-id)~g" $FILEPATH/config.json
-sed -i "s~value_2~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-name)~g" $FILEPATH/config.json
-sed -i "s~value_3~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-access-key-id)~g" $FILEPATH/config.json
-sed -i "s~value_4~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-secret-access-key)~g" $FILEPATH/config.json
-sed -i "s~value_5~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-relay-attach)~g" $FILEPATH/config.json
-sed -i "s~value_6~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-relay-address)~g" $FILEPATH/config.json
-sed -i "s~value_7~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-site-id)~g" $FILEPATH/config.json
 
+sed -i "s~value_1~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/id?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $FILEPATH/config.json
+sed -i "s~value_2~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/name?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $FILEPATH/config.json
+sed -i "s~value_3~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/access-key-id?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $FILEPATH/config.json
+sed -i "s~value_4~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/secret-access-key?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $FILEPATH/config.json
+sed -i "s~value_5~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/relay-attach?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $FILEPATH/config.json
+sed -i "s~value_6~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/relay-address?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $FILEPATH/config.json
+sed -i "s~value_7~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/site-id?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $FILEPATH/config.json
+sed -i "s~value_8~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/relay-id?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $FILEPATH/config.json
+sed -i "s~value_9~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/wc-urld?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $FILEPATH/config.json
 cat $FILEPATH/config.json
 
 cat >$FILEPATH/instance_name << 'EOL'
 instance_name
 EOL
 
-sed -i "s~instance_name~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-name)~g" $FILEPATH/instance_name
+sed -i "s~instance_name~${INSTANCE_NAME}~g" $FILEPATH/instance_name
 
 curl -L bit.ly/hostagent | sudo bash -s ${BUILD_ENV}
 host-agent-cli start
