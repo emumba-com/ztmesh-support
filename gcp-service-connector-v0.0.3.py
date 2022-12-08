4d83a7f27c81beba61436a34a0df31cbd12ad0eb
zeeshan50522 <zeeshanwaheed50522@gmail.com>
2022-12-08 14:51:24 +0500

    gcp python

---
+++ gcp-service-connector-v0.0.3.py
@@ -0,0 +1,98 @@
+# Copyright 2016 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Creates a Compute Instance with the provided metadata."""
+
+
+COMPUTE_URL_BASE = 'https://www.googleapis.com/compute/v1/'
+
+
+def GlobalComputeUrl(project, collection, name):
+  return ''.join([COMPUTE_URL_BASE, 'projects/', project,
+                  '/global/', collection, '/', name])
+
+def SubNetworksComputeUrl(project, region, name):
+  return ''.join([COMPUTE_URL_BASE, 'projects/', project,
+                  '/regions/', region, '/subnetworks/', name])
+
+def ZonalComputeUrl(project, zone, collection, name):
+  return ''.join([COMPUTE_URL_BASE, 'projects/', project,
+                  '/zones/', zone, '/', collection, '/', name])
+
+
+def GenerateConfig(context):
+  """Generate configuration."""
+
+  name_prefix = context.env['deployment']
+
+  instance = {
+      'zone': context.properties['zone'],
+      'machineType': ZonalComputeUrl(
+          context.env['project'], context.properties['zone'], 'machineTypes', context.properties['machine-type']),
+      'metadata': {
+          'items': [
+            {
+              'key': 'startup-script',
+              'value': context.properties['startup-script']
+            },
+            {
+              'key': 'block-project-ssh-keys',
+              'value': "true"
+            },
+            {
+              'key': 'ssh-keys',
+              'value': context.properties['ssh-keys']
+            }
+          ]
+      },
+       "serviceAccounts": [
+        {
+          "email": context.properties['sa-email'],
+          "scopes": [
+            "https://www.googleapis.com/auth/cloud-platform"
+          ]
+        }
+      ],
+      'disks': [{
+          'deviceName': 'boot',
+          'type': 'PERSISTENT',
+          'autoDelete': True,
+          'boot': True,
+          'initializeParams': {
+              'diskName': name_prefix + '-disk',
+              'sourceImage': ''.join([COMPUTE_URL_BASE, 'projects/ubuntu-os-cloud/global/images/family/ubuntu-2004-lts']) # https://cloud.google.com/compute/docs/images/os-details#ubuntu_lts
+              },
+          }],
+      'networkInterfaces': [{
+          'accessConfigs': [{
+              'name': 'external-nat',
+              'type': 'ONE_TO_ONE_NAT'
+              }],
+          'network': GlobalComputeUrl(
+              context.env['project'], 'networks', context.properties['vpc']),
+          'subnetwork': SubNetworksComputeUrl(
+              context.env['project'], context.properties['region'], context.properties['subnet_name']),
+          }]
+      }
+
+  # Resources to return.
+  resources = {
+      'resources': [{
+          'name': name_prefix + '-vm',
+          'type': 'compute.v1.instance',
+          'properties': instance
+          }]
+      }
+
+  return resources
