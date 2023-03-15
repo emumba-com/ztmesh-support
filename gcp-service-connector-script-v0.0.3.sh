b4f200ddfc1a4b27e593f01da5f2cc7df2105a8f
Syed Faraz Ahmed <faraz.ahmed@emumba.com>
2023-03-15 17:50:09 +0500

    Added new parameters to gcp connector script

---
+++ gcp-service-connector-script-v0.0.3.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+IDENTIFIER=$1
+BUILD_ENV=$2
+HOST_AGENT_INSTALL_SCRIPT_URL=$3
+INSTALL_MONITORING_TOOLS=$4
+LOG_LEVEL=$5
+
+FILEPATH=/root/.host-agent
+
+apt update -y
+
+echo "Creating config.json in path: ${FILEPATH}"
+
+mkdir -p $FILEPATH
+cat >$FILEPATH/config.json << 'EOL'
+{
+  "id": "value_1",
+  "name": "value_2",
+  "login_config": {
+    "access_key_id": "value_3",
+    "secret_access_key": "value_4"
+  },
+  "relay_config": {
+    "relay": "value_5",
+    "endpoint": "value_6",
+    "id" : "value_8"   
+  },
+  "site_config": {
+    "id": "value_7"
+  },
+  "wc_url": "value_9",
+  "logging": {
+    "level": "${LOG_LEVEL}",
+    "max_age": "",
+    "max_size": "",
+    "max_backups": "",
+    "compress": ""
+  },
+}
+EOL
+
+
+sed -i "s~value_1~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-id)~g" $FILEPATH/config.json
+sed -i "s~value_2~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-name)~g" $FILEPATH/config.json
+sed -i "s~value_3~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-access-key-id)~g" $FILEPATH/config.json
+sed -i "s~value_4~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-secret-access-key)~g" $FILEPATH/config.json
+sed -i "s~value_5~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-relay-attach)~g" $FILEPATH/config.json
+sed -i "s~value_6~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-relay-address)~g" $FILEPATH/config.json
+sed -i "s~value_7~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-site-id)~g" $FILEPATH/config.json
+sed -i "s~value_8~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-relay-id)~g" $FILEPATH/config.json
+sed -i "s~value_9~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-wc-url)~g" $FILEPATH/config.json
+cat $FILEPATH/config.json
+
+cat >$FILEPATH/instance_name << 'EOL'
+instance_name
+EOL
+
+sed -i "s~instance_name~$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-name)~g" $FILEPATH/instance_name
+
+WEB_CONTROLLER_URL=$(gcloud secrets versions access "latest" --secret=$IDENTIFIER-wc-url)
+
+curl -L ${HOST_AGENT_INSTALL_SCRIPT_URL} | sudo bash -s ${BUILD_ENV} ${WEB_CONTROLLER_URL} ${INSTALL_MONITORING_TOOLS}
+host-agent-cli start
