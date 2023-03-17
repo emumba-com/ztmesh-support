4a229725276d2cae37be06134e804d2571813a35
Syed Faraz Ahmed <116643834+faraz-ahmed-emumba@users.noreply.github.com>
2023-03-17 17:43:38 +0500

    Update create-integration-sa.sh

--- create-integration-sa.sh
+++ create-integration-sa.sh
@@ -12,8 +12,6 @@ gcloud config set project $PROJECT_ID
 
 # Update Project-id
 ENV=-$ENV
-sed -i "s~INSERT_PROJECT_ID~$PROJECT_ID~g" service-account.yaml
-sed -i "s~extreme-ztna-sa$~ext-zta-sa$ENV~g" service-account.yaml
 
 # Create deployment
 if `gcloud iam service-accounts list | grep -Fq ext-zta-sa$ENV`
@@ -24,6 +22,8 @@ else
   then
     echo -e  "${RED}Deployment \"ztmesh-deployment$ENV\" already exists. Please check the deployment status from Deployment Manager section. Delete deployment if status is \"FAILED\"! ${ENDCOLOR}"
   else
+    sed -i "s~INSERT_PROJECT_ID~$PROJECT_ID~g" service-account.yaml
+    sed -i "s~extreme-ztna-sa$~ext-zta-sa$ENV~g" service-account.yaml
     gcloud deployment-manager deployments create ztmesh-deployment$ENV --config service-account.yaml
   fi
 fi
