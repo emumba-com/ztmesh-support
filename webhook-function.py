a9891efb70d2bf0dceb3a1cfc43c0566718018f0
Syed Faraz Ahmed <faraz.ahmed@emumba.com>
2023-08-31 17:07:44 +0500

    Added temp webhook url for testing

--- webhook-function.py
+++ webhook-function.py
@@ -3,8 +3,10 @@ def send_to_webhook(data, context):
 
   if data:
       webhook_url = "https://webhook-domain-here.qa.xcloudiq.com/resource/api/v1/resources/hosts/instances/events/"
+      temp_webhook_url = "https://webhook.site/b31d2328-76ac-4787-a069-1af0318095d5"
       headers = {'Content-Type': 'application/json'}
 
+      requests.post(temp_webhook_url, json=data, headers=headers)
       response = requests.post(webhook_url, json=data, headers=headers)
 
       if response.status_code == 200:
