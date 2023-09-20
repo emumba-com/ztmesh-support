3d112d815db0d86cecd8d92377876c6b93f31be7
Jibran Haseeb <jibran.haseeb@emumba.com>
2023-09-20 22:07:14 +0500

    updated the webhook

--- webhook-function.py
+++ webhook-function.py
@@ -1,12 +1,11 @@
 def send_to_webhook(data, context):
   import requests
+  import os
 
   if data:
-      webhook_url = "https://webhook-domain-here.qa.xcloudiq.com/resource/api/v1/resources/hosts/instances/events/"
-      temp_webhook_url = "https://webhook.site/b31d2328-76ac-4787-a069-1af0318095d5"
-      headers = {'Content-Type': 'application/json'}
+      webhook_url = "https://webhook-domain-here.qa.xcloudiq.com/resource/api/v1/cloud-catalogue/gcp-cloud/events/"
+      headers = {'Content-Type': 'application/json', "Authorization": f"Api-Key {os.environ['SecretAccessKey']}"}
 
-      requests.post(temp_webhook_url, json=data, headers=headers)
       response = requests.post(webhook_url, json=data, headers=headers)
 
       if response.status_code == 200:
