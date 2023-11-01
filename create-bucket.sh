ad4d6953f3b5bc7371d5be749d6fadce613df00e
Syed Faraz Ahmed <116643834+syed-farazahmed@users.noreply.github.com>
2023-11-01 17:01:54 +0500

    Update create-bucket.sh

--- create-bucket.sh
+++ create-bucket.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+GREEN="\e[32m"
+RED="\e[31m"
+ENDCOLOR="\e[0m"
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 PROJECT_ID ENV WORKSPACE_ID"
+    exit 1
+fi
 
 PROJECT_ID=$1
 ENV=$2
 WORKSPACE_ID=$3
 
-GREEN="\e[32m"
-RED="\e[31m"
-ENDCOLOR="\e[0m"
+if ! [[ "$WORKSPACE_ID" =~ ^[0-9]+$ ]]; then
+    echo "WORKSPACE_ID must be a numeric value."
+    exit 1
+fi
 
 # Set project for command-line tool
 gcloud config set project $PROJECT_ID
@@ -24,12 +33,12 @@ else
   then
     echo -e  "${RED}Deployment \"extreme-zta-bucket$ENV-${WORKSPACE_ID}\" already exists. Please check the deployment status from Deployment Manager section. Delete deployment if status is \"FAILED\"! ${ENDCOLOR}"
   else
-    sed -i "s~extreme-zta-bucket$~extreme-zta-bucket$ENV-${WORKSPACE_ID}~g" create-bucket.yaml
+    sed -i "s~extreme-zta-bucket$~extreme-zta-bucket-${PROJECT_ID}$ENV-${WORKSPACE_ID}~g" create-bucket.yaml
     gcloud deployment-manager deployments create extreme-zta-bucket$ENV-${WORKSPACE_ID} --config create-bucket.yaml
     sed "s~webhook-domain-here~$SUBDOMAIN~g" webhook-function.py > main.py
     zip function-code.zip main.py requirements.txt
-    gsutil cp function-code.zip gs://extreme-zta-bucket$ENV-${WORKSPACE_ID}/
-    gsutil acl ch -u AllUsers:R gs://extreme-zta-bucket$ENV-${WORKSPACE_ID}/function-code.zip
-    echo -e  "${GREEN}ZIP Archive URL: gs://extreme-zta-bucket$ENV-${WORKSPACE_ID}/function-code.zip${ENDCOLOR}"
+    gsutil cp function-code.zip gs://extreme-zta-bucket-${PROJECT_ID}$ENV-${WORKSPACE_ID}/
+    gsutil acl ch -u AllUsers:R gs://extreme-zta-bucket-${PROJECT_ID}$ENV-${WORKSPACE_ID}/function-code.zip
+    echo -e  "${GREEN}ZIP Archive URL: gs://extreme-zta-bucket-${PROJECT_ID}$ENV-${WORKSPACE_ID}/function-code.zip${ENDCOLOR}"
   fi
 fi
